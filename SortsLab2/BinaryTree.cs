4692f4c8852e2ebf9739aa1ba1da109b37b89ac1
stlad <82213506+stlad@users.noreply.github.com>
2022-03-01 21:44:50 +0500

    класс дерева

---
+++ SortsLab2/BinaryTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortsLab2
+{
+    public class BinaryTree
+    {
+        public BinaryTree Left { get; set; }
+        public BinaryTree Right { get; set; }
+        public char key { get; set; }
+        
+    }
+
+
+}
