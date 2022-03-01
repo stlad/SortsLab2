00a3c095abee3780fd3921156448284dd4d483a8
stlad <82213506+stlad@users.noreply.github.com>
2022-03-01 17:39:29 +0500

    поле счетчик итерраций

--- SortsLab2/Experiment.cs
+++ SortsLab2/Experiment.cs
@@ -15,11 +15,13 @@ namespace SortsLab2
         public string Text { get; set; }
         public int Length => Text.Length;
 
+        public int Iterations;
         //public Dictionary<Sorter.SortType, int > SortResults { get; set; }
 
         public Experiment() { }
         public Experiment(string text)
         {
+            Iterations = 0;
             Text = text;
             //SortResults = new Dictionary<Sorter.SortType, int>();
             //SortResults[Sorter.SortType.Bubble] = 0;
@@ -30,6 +32,13 @@ namespace SortsLab2
             //SortResults[Sorter.SortType.HeapTree] = 0;
             //SortResults[Sorter.SortType.Radix] = 0;
             //SortResults[Sorter.SortType.RedBlackTree] = 0;
+        }
+
+
+        public void MakeSort(Sorter.SortType sortType)
+        {
+            var res =Sorter.Sorts[sortType](this);
+            Console.WriteLine(res);
         }
 
         /// <summary>
