4deaa48055efa1bead5430ce3b7bd6e3c1464d56
stlad <82213506+stlad@users.noreply.github.com>
2022-03-14 18:08:55 +0500

    Сериализация, работа лаборатории есть.

--- SortsLab2/Experiment.cs
+++ SortsLab2/Experiment.cs
@@ -13,7 +13,7 @@ namespace SortsLab2
     public class Experiment
     {
         public string Text { get; set; }
-        public int Length => Text.Length;
+        public int Length { get; set; }
 
         //public Dictionary<Sorter.SortType, int > SortIterations { get; set; }
 
@@ -22,12 +22,13 @@ namespace SortsLab2
         public Experiment(string text)
         {
             Text = text;
+            Length = text.Length;
             SortIterations = new int[8];
 
             SortIterations[(int)Sorter.SortType.Bubble] = 0;
             SortIterations[(int)Sorter.SortType.QSort] = 0;
             SortIterations[(int)Sorter.SortType.SortTree] = 0;
-            SortIterations[(int)Sorter.SortType.Instert] = 0;
+            SortIterations[(int)Sorter.SortType.Insert] = 0;
             SortIterations[(int)Sorter.SortType.MergeSort] = 0;
             SortIterations[(int)Sorter.SortType.HeapTree] = 0;
             SortIterations[(int)Sorter.SortType.Radix] = 0;
@@ -46,12 +47,35 @@ namespace SortsLab2
         /// </summary>
         public void MakeAllSorts()
         {
+            Console.WriteLine($"строка: {this.Text}");
+            Console.WriteLine($"длина строки: {this.Length}");
+            Console.WriteLine("\n\n\n\n");
+
+            int index =0;
             foreach(var sort in Sorter.Sorts.Values)
             {
+                Console.WriteLine($"Сортировка: {(Sorter.SortType)index}");
                 var st = sort(this);
                 Console.WriteLine(st);
+                Console.WriteLine($"Кол-во итерраций: {SortIterations[index]}");
+                index++;
+                Console.WriteLine("-----------------------------------------------------\n\n");
             }
         }
 
+
+        /// <summary>
+        /// Все сортировки без консольного интерфейса
+        /// </summary>
+        public void MakeAllSortsQuiet()
+        {
+            foreach (var sort in Sorter.Sorts.Values)
+            {
+                var st = sort(this);
+                //Console.WriteLine(st);
+            }
+        }
     }
+
+    
 }
--- SortsLab2/Laboratory.cs
+++ SortsLab2/Laboratory.cs
@@ -13,31 +13,46 @@ namespace SortsLab2
     public class Laboratory
     {
         public List<Experiment> Experiments = new List<Experiment>();
-        public void Serialyze()
+
+        public Laboratory(List<string> exps)
+        {
+            foreach(var str in exps)
+                Experiments.Add(new Experiment(str));
+        }
+
+        public void MakeAllExperiments()
+        {
+            foreach(var exp in Experiments)
+            {
+                exp.MakeAllSortsQuiet();
+            }
+        }
+
+        public void Serialyze(string fileName)
         {
             var writer = new System.Xml.Serialization.XmlSerializer(typeof(List<Experiment>));
-            System.IO.StreamWriter file = new System.IO.StreamWriter("Experiments.xml");
+            System.IO.StreamWriter file = new System.IO.StreamWriter(fileName);
             writer.Serialize(file, Experiments);
         }
 
-        public static void Xmltry()
-        {
-            var b = new Book();
-            b.Title = "Lord of the rings";
-            b.a.Add(1);
-            b.a.Add(6);
-            b.a.Add(5);
-            b.a.Add(2);
-            var writer = new System.Xml.Serialization.XmlSerializer(typeof(Book));
-            System.IO.StreamWriter sw = new System.IO.StreamWriter("books.xml");
+        //private static void Xmltry()
+        //{
+        //    var b = new Book();
+        //    b.Title = "Lord of the rings";
+        //    b.a.Add(1);
+        //    b.a.Add(6);
+        //    b.a.Add(5);
+        //    b.a.Add(2);
+        //    var writer = new System.Xml.Serialization.XmlSerializer(typeof(Book));
+        //    System.IO.StreamWriter sw = new System.IO.StreamWriter("books.xml");
 
-            writer.Serialize(sw, b);
-            sw.Close();
+        //    writer.Serialize(sw, b);
+        //    sw.Close();
 
-            //var reader = new System.Xml.Serialization.XmlSerializer(typeof(Book));
-            //var file = new System.IO.StreamReader("books.xml");
-            //Book book = (Book)reader.Deserialize(file);
-            //file.Close();
-        }
+        //    //var reader = new System.Xml.Serialization.XmlSerializer(typeof(Book));
+        //    //var file = new System.IO.StreamReader("books.xml");
+        //    //Book book = (Book)reader.Deserialize(file);
+        //    //file.Close();
+        //}
     }
 }
